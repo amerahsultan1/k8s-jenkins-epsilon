2e1041fb56debc4ef921bdd9f7d9928e923f8cbe
Amerah Almutairi <66031162+amerahsultan1@users.noreply.github.com>
2020-08-30 11:48:19 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0] -p 80:80@loadbalancer
 kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.yaml
 kubectl create -n jenkins clusterrolebinding jenkins-account --clusterrole=cluster-admin --serviceaccount=jenkins:jenkins
 WAIT=90
